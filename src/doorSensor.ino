b8b661798b98a55584fec73a067826ebb7d50762
Splocker <sethaplatt@gmail.com>
2020-07-15 20:50:17 -0500

    While loop removal, revertin to conditionals.

--- src/doorSensor.ino
+++ src/doorSensor.ino
@@ -1,10 +1,3 @@
-/*
- * Project doorSensor
- * Description:
- * Author:
- * Date:
- */
-
  //The average open-close entry period.
  #define AverageEntryTime 7
  //The door open alert period in seconds.
@@ -38,102 +31,52 @@
 
  void loop() {
 
-      while(!doorOpen){
-        if(analogRead(reed) > 4000){
-            //Setting the doorOpen flag to true to reflect the door being open.
-            doorOpen = true;
-            //Setting trigger time to the current time for notification tracking.
-            triggerTime = Time.now();
-            //Publishing an event to recognize door state change to open.
-            Particle.publish("Door_open", NULL, 0, PRIVATE);
-        }
-      }
-
-      while(doorOpen){
-        if((analogRead(reed) < 4000)){
-            //Setting doorOpen flag to flase to reflect the door being closed.
-            doorOpen = false;
-            //Saving the time the door was open for.
-            openPeriod = doorOpenPeriod(Time.now());
-            //If the door was closed within the OpenAlertTime a "Your door WAS opened" notification is sent.
-            if(openPeriod <= AverageEntryTime){
-                //Publishing a quick open-close event with the openPeriod value.
-                Particle.publish("Your_door_was_opened", String::format("%ld", openPeriod), 0, PRIVATE);
-            }
-            else{
-                //Publishing an extended period door close event with the openPeriod value.
-                Particle.publish("Your_door_is_now_closed", String::format("%ld", openPeriod), 0, PRIVATE);
-            }
-            //Resetting lastTimeNotification vaiable for propper notification behavior.
-            lastTimeNotificationIndex = 0;
-            //Resetting doorOpenNotification flag.
-            doorOpenNotification = false;
-        }
-        else if((doorOpenPeriod(Time.now()) / OpenAlertTime) > lastTimeNotificationIndex){
-            //Saving the interval calculation for ease of code reading.
-            AlertIntervals = doorOpenPeriod(Time.now()) / OpenAlertTime;
-            //Set DoorOpenNotificationTime for IFTTT notification data pull.
-            DoorOpenNotificationTime = AlertIntervals * openAlertPeriod;
-            //Particle event publish your door has been openfor x minutes.
-            Particle.publish("Extended_open_period", String::format("%d", AlertIntervals * openAlertPeriod), 0, PRIVATE);
-            //Increment last time notification.
-            lastTimeNotificationIndex++;
-        }
-        else if(!doorOpenNotification && (doorOpenPeriod(Time.now()) > AverageEntryTime)){
-            Particle.publish("Your_door_is_open", NULL, 0, PRIVATE);
-            //Setting doorOpen Notification to true to prevent duplicate notifications.
-            doorOpenNotification = true;
-        }
-      }
-
-
-
-    //  //Door open conditional
-    //  if((analogRead(reed) > 4000) && !doorOpen){
-    //      Particle.publish("Door_open", NULL, 0, PRIVATE);
-    //      //Setting the doorOpen flag to true to reflect the door being open.
-    //      doorOpen = true;
-    //      //Setting trigger time to the current time for notification tracking.
-    //      triggerTime = Time.now();
-    //  }
-    //  //Door closed conditional
-    //  else if((analogRead(reed) < 4000) && doorOpen){
-    //      //Setting doorOpen flag to flase to reflect the door being closed.
-    //      doorOpen = false;
-    //      //Saving the time the door was open for.
-    //      openPeriod = doorOpenPeriod(Time.now());
-    //      //If the door was closed within the OpenAlertTime a "Your door WAS opened" notification is sent.
-    //      if(openPeriod <= AverageEntryTime){
-    //          //Publishing a quick open-close event with the openPeriod value.
-    //          Particle.publish("Your_door_was_opened", String::format("%ld", openPeriod), 0, PRIVATE);
-    //      }
-    //      else{
-    //          //Publishing an extended period door close event with the openPeriod value.
-    //          Particle.publish("Your_door_is_now_closed", String::format("%ld", openPeriod), 0, PRIVATE);
-    //      }
-    //      //Resetting lastTimeNotification vaiable for propper notification behavior.
-    //      lastTimeNotificationIndex = 0;
-    //      //Resetting doorOpenNotification flag.
-    //      doorOpenNotification = false;
-    //  }
-    //  //Otherwise Notification conditional.
-    //  else if(((doorOpenPeriod(Time.now()) / OpenAlertTime) > lastTimeNotificationIndex) && doorOpen){
-    //      //Saving the interval calculation for ease of code reading.
-    //      AlertIntervals = doorOpenPeriod(Time.now()) / OpenAlertTime;
-    //      //Set DoorOpenNotificationTime for IFTTT notification data pull.
-    //      DoorOpenNotificationTime = AlertIntervals * openAlertPeriod;
-    //      //Particle event publish your door has been openfor x minutes.
-    //      Particle.publish(String::format("Your_door_has_been_open_for_%d_minutes", AlertIntervals * openAlertPeriod), "Extended_open_period", 0, PRIVATE);
-    //      //Increment last time notification.
-    //      lastTimeNotificationIndex++;
-    //  }
-    //  else{
-    //      //If the door has been open for 7 seconds and a door open notification has not been sent one will be sent.
-    //      if(doorOpen && (doorOpenPeriod(Time.now()) > AverageEntryTime) && !doorOpenNotification){
-    //          Particle.publish("Your_door_is_open", NULL, 0, PRIVATE);
-    //          doorOpenNotification = true;
-    //      }
-    //  }
+     //Door open conditional
+     if((analogRead(reed) > 4000) && !doorOpen){
+         Particle.publish("Door_open", NULL, 0, PRIVATE);
+         //Setting the doorOpen flag to true to reflect the door being open.
+         doorOpen = true;
+         //Setting trigger time to the current time for notification tracking.
+         triggerTime = Time.now();
+     }
+     //Door closed conditional
+     else if((analogRead(reed) < 4000) && doorOpen){
+         //Setting doorOpen flag to flase to reflect the door being closed.
+         doorOpen = false;
+         //Saving the time the door was open for.
+         openPeriod = doorOpenPeriod(Time.now());
+         //If the door was closed within the OpenAlertTime a "Your door WAS opened" notification is sent.
+         if(openPeriod <= AverageEntryTime){
+             //Publishing a quick open-close event with the openPeriod value.
+             Particle.publish("Your_door_was_opened", String::format("%ld", openPeriod), 0, PRIVATE);
+         }
+         else{
+             //Publishing an extended period door close event with the openPeriod value.
+             Particle.publish("Your_door_is_now_closed", String::format("%ld", openPeriod), 0, PRIVATE);
+         }
+         //Resetting lastTimeNotification vaiable for propper notification behavior.
+         lastTimeNotificationIndex = 0;
+         //Resetting doorOpenNotification flag.
+         doorOpenNotification = false;
+     }
+     //Otherwise Notification conditional.
+     else if(((doorOpenPeriod(Time.now()) / OpenAlertTime) > lastTimeNotificationIndex) && doorOpen){
+         //Saving the interval calculation for ease of code reading.
+         AlertIntervals = doorOpenPeriod(Time.now()) / OpenAlertTime;
+         //Set DoorOpenNotificationTime for IFTTT notification data pull.
+         DoorOpenNotificationTime = AlertIntervals * openAlertPeriod;
+         //Particle event publish your door has been openfor x minutes.
+         Particle.publish("Extended_open_period", String::format("%d", AlertIntervals * openAlertPeriod), 0, PRIVATE);
+         //Increment last time notification.
+         lastTimeNotificationIndex++;
+     }
+     else{
+         //If the door has been open for 7 seconds and a door open notification has not been sent one will be sent.
+         if(doorOpen && (doorOpenPeriod(Time.now()) > AverageEntryTime) && !doorOpenNotification){
+             Particle.publish("Your_door_is_open", NULL, 0, PRIVATE);
+             doorOpenNotification = true;
+         }
+     }
  }
 
  long int doorOpenPeriod(long int time){
